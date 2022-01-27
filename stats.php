db09f28fdfedf352c11867d6d2113c2783ceff9d
Brook Miller <millerbrook@gmail.com>
2022-01-27 05:47:48 -0500

    barebones html and js for charts and bootstrap on sjd dash

---
+++ stats.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <body>
+    <div class="row">
+        <div class="col-sm">See mystery to mathematicks fly.
+            Pope.</div>
+        <div class="col-sm">Statistics dashboard</div>
+        <div class="col-sm">Choose how many records to view</div>
+        </div>
+<div class="container">
+    <div class="row">
+    <div id="jsGrid" class="col-sm-4">col-sm-8</div>
+    <div class="col-sm-4">
+        <canvas id="MyChart">
+
+        </canvas>
+    </div>
+    </div>
+    
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
+</body>
+</html>
+
